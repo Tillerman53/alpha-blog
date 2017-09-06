df1da8da58e41e55988a4af067827faae2cf80e4
Rails Udemy Course <RailsUdemyCourse@gmail.com>
2017-09-06 16:53:04 +0000

    add user data to articles listing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
    validates :username, presence: true, 
              uniqueness: true, 
              length: { minimum: 3, maximum: 25 } 
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
    validates :email, presence: true, length: { maximum: 105 },
              uniqueness: {case_sensitive: false },
              format: { with: VALID_EMAIL_REGEX }
